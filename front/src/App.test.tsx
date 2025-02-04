cb15913fd5693c8839f2ceb808d66792a3c3a33c
kazushi takahashi <kazushi@rvm.jp>
2025-02-04 14:41:29 +0900

    テストが動く環境が整った

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { ToastContainer } from "react-toastify";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("エラーが発生したらトーストが表示される", async () => {
+  render(
+    <>
+      <App />
+      <ToastContainer />
+    </>
+  );
+
+  // 発注者が空欄のまま処理を実行
+  const fileInput = screen.getByLabelText("ファイルを選択"); // inputタグに `aria-label="ファイルを選択"` を付けると良い
+  const fakeFile = new File([""], "test.csv", { type: "text/csv" });
+
+  await userEvent.upload(fileInput, fakeFile);
+
+  const errorToast = await screen.findByText("発注者が空欄だよ！お兄ちゃん！");
+  expect(errorToast).toBeInTheDocument();
 });
